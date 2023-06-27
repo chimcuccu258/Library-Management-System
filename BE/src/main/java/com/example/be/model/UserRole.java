88df759765e3b8d361988fc5e1f1a155fa345dc4
Minh Nga <vuminhnga25@gmail.com>
2023-06-27 13:10:04 +0700

    LMS first commit

---
+++ BE/src/main/java/com/example/be/model/UserRole.java
@@ -0,0 +1,31 @@
+package com.example.be.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "USER_ROLE")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRole {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  //  @Id
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  @JsonIgnore
+  private User user;
+
+  //  @Id
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "role_id")
+  private Role role;
+}
---
+++ BE/src/main/java/com/example/be/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.be.repository;
+
+import com.example.be.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  User findByEmail(String email);
+
+  boolean existsByPhoneNumber(String phoneNumber);
+
+  boolean existsByEmail(String email);
+
+}
